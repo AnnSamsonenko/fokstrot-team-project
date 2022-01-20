e99812da4ad8593f892e702bdd563ce5c554fc00
Anna Samsonenko <foster.anna93@gmail.com>
2022-01-20 02:25:01 +0200

    task-4-modal-samsonenko
    1.  add Controller and View for modal
    2. add render modal through Publisher
    3. add close modal functionality (click, keypress)
    4.no styles 😁😂

---
+++ app/modal/controller_modal.js
@@ -0,0 +1,17 @@
+import { ViewModal } from './view_modal.js';
+import { Publisher } from '../publisher.js';
+
+export class ControllerModal {
+  constructor() {
+    this.view = new ViewModal();
+    this.pub = new Publisher();
+
+    this.pub.subscribe('ON_MODAL_CLICK', this.handleClickOpenNodal);
+  }
+
+  handleClickOpenNodal = obj => {
+    this.view.renderModal(obj);
+    this.view.showModal();
+    this.view.addListenersForCloseModal();
+  };
+}
